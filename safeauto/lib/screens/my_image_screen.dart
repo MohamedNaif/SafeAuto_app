4e0696224c52fcfef1676d68c6952d02ea1c76b3
mohamed naif <naifm371@gmail.com>
2024-02-17 03:12:16 +0200

    user profile and fireStorage

--- safeauto/lib/screens/my_image_screen.dart
+++ safeauto/lib/screens/my_image_screen.dart
@@ -1,26 +1,92 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
-class SelectedImageScreen extends StatelessWidget {
-  final List<File> imageFiles;
+class SelectedImageScreen extends StatefulWidget {
+  final List<String> imageUrls;
 
-  const SelectedImageScreen({Key? key, required this.imageFiles,}) : super(key: key);
+  const SelectedImageScreen({Key? key, required this.imageUrls})
+      : super(key: key);
 
+  @override
+  _SelectedImageScreenState createState() => _SelectedImageScreenState();
+}
+
+class _SelectedImageScreenState extends State<SelectedImageScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Selected Images'),
       ),
-      body: ListView.builder(
-        itemCount: imageFiles.length,
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2, // You can adjust the number of columns here
+          crossAxisSpacing: 8,
+          mainAxisSpacing: 8,
+        ),
+        itemCount: widget.imageUrls.length,
         itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Image.file(imageFiles[index]),
-          );
+          return _buildImageItem(index);
         },
       ),
     );
+  }
+
+  Widget _buildImageItem(int index) {
+    return Stack(
+      children: [
+        FutureBuilder(
+          future: _getImage(widget.imageUrls[index]),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            }
+            if (snapshot.hasError) {
+              return Center(child: Text('Error: ${snapshot.error}'));
+            }
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image(
+                image: NetworkImage(snapshot.data.toString()),
+                fit: BoxFit.cover,
+              ),
+            );
+          },
+        ),
+        Positioned(
+          top: 4,
+          right: 4,
+          child: IconButton(
+            icon: Icon(Icons.delete),
+            onPressed: () => _deleteImage(index),
+            color: Colors.red,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Future<String> _getImage(String imageUrl) async {
+    try {
+      // Fetch image from Firebase Storage
+      var ref = FirebaseStorage.instance.refFromURL(imageUrl);
+      var url = await ref.getDownloadURL();
+      return url;
+    } catch (e) {
+      print("Error fetching image from Firebase Storage: $e");
+      return ''; // Return empty string in case of error
+    }
+  }
+
+  Future<void> _deleteImage(int index) async {
+    try {
+      var ref = FirebaseStorage.instance.refFromURL(widget.imageUrls[index]);
+      await ref.delete();
+      setState(() {
+        widget.imageUrls.removeAt(index);
+      });
+    } catch (e) {
+      print("Error deleting image from Firebase Storage: $e");
+    }
   }
 }
--- safeauto/lib/screens/user_profile.dart
+++ safeauto/lib/screens/user_profile.dart
@@ -21,6 +21,38 @@ class UserProfileScreen extends StatefulWidget {
 
 class _UserProfileScreenState extends State<UserProfileScreen> {
   List<File> _selectedImages = [];
+  List<String> _imageUrls = [];
+
+  @override
+  void initState() {
+    super.initState();
+    // Fetch and set images URLs when the widget is initialized
+    fetchAndSetImageUrls();
+  }
+
+  Future<void> fetchAndSetImageUrls() async {
+    try {
+      // Get the current user's ID
+      var userId = FirebaseAuth.instance.currentUser!.uid;
+
+      // Get a reference to the folder where user's images are stored
+      var refStorage = FirebaseStorage.instance.ref().child('users/$userId');
+
+      // List all items in the folder
+      var result = await refStorage.listAll();
+
+      // Iterate through the items and fetch download URLs
+      for (var imageRef in result.items) {
+        var url = await imageRef.getDownloadURL();
+        _imageUrls.add(url);
+      }
+
+      // Update the UI
+      setState(() {});
+    } catch (e) {
+      print("Failed to fetch image URLs from Firebase Storage: $e");
+    }
+  }
 
   Future<void> getImage(ImageSource source) async {
     final ImagePicker picker = ImagePicker();
@@ -34,9 +66,11 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
 
   Future<void> uploadImagesToFirebaseStorage() async {
     try {
+      var userId = FirebaseAuth.instance.currentUser!.uid;
       for (var imageFile in _selectedImages) {
-        String imageName = imageFile.path.split('/').last;
-        var refStorage = FirebaseStorage.instance.ref().child(imageName);
+        String imageName = basename(imageFile.path);
+        var refStorage =
+            FirebaseStorage.instance.ref().child('users/$userId/$imageName');
         await refStorage.putFile(imageFile);
         String downloadURL = await refStorage.getDownloadURL();
         print("Image uploaded to Firebase Storage. Download URL: $downloadURL");
@@ -48,6 +82,10 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
 
   @override
   Widget build(BuildContext context) {
+    // Get current user
+    User? user = FirebaseAuth.instance.currentUser;
+    String? email = user?.email ?? '';
+
     return Scaffold(
       backgroundColor: const Color(0xff030A10),
       appBar: AppBar(
@@ -106,8 +144,8 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
                             radius: 55,
                             child: CircleAvatar(
                               radius: 50,
-                              backgroundImage: AssetImage(
-                                  'assets/images/photo_2023-12-14_16-40-34.jpg'),
+                              // backgroundImage: AssetImage(
+                              //     'assets/images/photo_2023-12-14_16-40-34.jpg'),
                             ),
                           ),
                           SizedBox(height: 20),
@@ -120,7 +158,7 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
                           ),
                           SizedBox(height: 10),
                           Text(
-                            emailController.text, // User's email
+                            email ?? '', // User's email
                             style: TextStyle(
                               fontSize: 18,
                             ),
@@ -154,6 +192,8 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
                                 onTap: () async {
                                   await getImage(ImageSource.gallery);
                                   await uploadImagesToFirebaseStorage();
+                                  // Fetch and set image URLs again
+                                  await fetchAndSetImageUrls();
                                   Navigator.pop(context);
                                 },
                               ),
@@ -163,6 +203,8 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
                                 onTap: () async {
                                   await getImage(ImageSource.camera);
                                   await uploadImagesToFirebaseStorage();
+                                  // Fetch and set image URLs again
+                                  await fetchAndSetImageUrls();
                                   Navigator.pop(context);
                                 },
                               ),
@@ -184,23 +226,14 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
                 SizedBox(height: 10),
                 ElevatedButton(
                   onPressed: () {
-                    if (_selectedImages.isNotEmpty) {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) =>
-                              SelectedImageScreen(imageFiles: _selectedImages),
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => SelectedImageScreen(
+                          imageUrls: _imageUrls,
                         ),
-                      );
-                    } else {
-                      AwesomeDialog(
-                        context: context,
-                        dialogType: DialogType.error,
-                        animType: AnimType.rightSlide,
-                        title: 'Error',
-                        desc: "لا يوجد صور لعرضها",
-                      )..show();
-                    }
+                      ),
+                    );
                   },
                   child: Text('View Selected Images'),
                 ),
@@ -212,222 +245,3 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
     );
   }
 }
-
-
-// class UserProfileScreen extends StatefulWidget {
-//   @override
-//   State<UserProfileScreen> createState() => _UserProfileScreenState();
-// }
-
-// class _UserProfileScreenState extends State<UserProfileScreen> {
-//   File? file;
-//   File? file1;
-//   String? url;
-
-//   List<String> imageUrls = [];
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     // Fetch and display images when the widget is initialized
-//     fetchAndSetImages();
-//   }
-
-//   Future<void> fetchAndSetImages() async {
-//     // Get the current user's ID
-//     var userId = FirebaseAuth.instance.currentUser!.uid;
-
-//     // Get a reference to the folder where user's images are stored
-//     var refStorage = FirebaseStorage.instance.ref().child('users/$userId');
-
-//     // List all items in the folder
-//     var result = await refStorage.listAll();
-
-//     // Iterate through the items and fetch download URLs
-//     for (var imageRef in result.items) {
-//       var url = await imageRef.getDownloadURL();
-//       imageUrls.add(url);
-//     }
-
-//     // Update the UI
-//     setState(() {});
-//   }
-
-//   Future<void> getImage(ImageSource source) async {
-//     final ImagePicker picker = ImagePicker();
-//     final XFile? image = await picker.pickImage(source: source);
-//     if (image != null) {
-//       setState(() {
-//         if (source == ImageSource.gallery) {
-//           file = File(image.path);
-//         } else if (source == ImageSource.camera) {
-//           file1 = File(image.path);
-//         }
-//       });
-
-//       var imageName = basename(image.path);
-
-//       // Get the current user's ID
-//       var userId = FirebaseAuth.instance.currentUser!.uid;
-
-//       // Create the storage reference with user's ID as part of the path
-//       var refStorage =
-//           FirebaseStorage.instance.ref().child('users/$userId/$imageName');
-//       await refStorage.putFile(file1!);
-
-//       // Fetch and display updated images
-//       await fetchAndSetImages();
-//     }
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       backgroundColor: const Color(0xff030A10),
-//       appBar: AppBar(
-//         backgroundColor: const Color(0xff030A10),
-//         title: Text(
-//           'User Profile',
-//           style: TextStyle(
-//             color: Colors.white,
-//             fontSize: 24,
-//             fontWeight: FontWeight.bold,
-//           ),
-//         ),
-//         centerTitle: true,
-//         actions: [
-//           IconButton(
-//             onPressed: () async {
-//               GoogleSignIn googleSignIn = GoogleSignIn();
-//               googleSignIn.disconnect();
-//               await FirebaseAuth.instance.signOut();
-//               Navigator.pushReplacement(
-//                 context,
-//                 MaterialPageRoute(
-//                   builder: (context) => LoginScreen(),
-//                 ),
-//               );
-//             },
-//             icon: Icon(
-//               Icons.exit_to_app,
-//               color: Colors.white,
-//               size: 25,
-//             ),
-//           ),
-//         ],
-//       ),
-//       body: SingleChildScrollView(
-//         child: Center(
-//           child: Padding(
-//             padding: EdgeInsets.all(16.0),
-//             child: Column(
-//               crossAxisAlignment: CrossAxisAlignment.center,
-//               children: <Widget>[
-//                 Container(
-//                   height: 350,
-//                   width: 300,
-//                   child: Card(
-//                     shape: RoundedRectangleBorder(
-//                         borderRadius: BorderRadius.all(Radius.circular(30))),
-//                     color: const Color(0xFF00E5F9),
-//                     child: Padding(
-//                       padding: EdgeInsets.all(20.0),
-//                       child: Column(
-//                         children: [
-//                           CircleAvatar(
-//                             backgroundColor: const Color(0xFF062A3A),
-//                             radius: 55,
-//                             child: CircleAvatar(
-//                               radius: 50,
-//                               backgroundImage: AssetImage(
-//                                   'assets/images/photo_2023-12-14_16-40-34.jpg'),
-//                             ),
-//                           ),
-//                           SizedBox(height: 20),
-//                           Text(
-//                             userName.text, // User's name
-//                             style: TextStyle(
-//                               fontSize: 24,
-//                               fontWeight: FontWeight.bold,
-//                             ),
-//                           ),
-//                           SizedBox(height: 10),
-//                           Text(
-//                             emailController.text, // User's email
-//                             style: TextStyle(
-//                               fontSize: 18,
-//                             ),
-//                           ),
-//                         ],
-//                       ),
-//                     ),
-//                   ),
-//                 ),
-//                 SizedBox(height: 50),
-//                 Text(
-//                   'Add a picture of the people allowed to use the car', // User's email
-//                   style: TextStyle(
-//                     color: Colors.white,
-//                     fontSize: 13,
-//                   ),
-//                 ),
-//                 SizedBox(height: 10),
-//                 ElevatedButton(
-//                   style: ButtonStyle(
-//                     backgroundColor: MaterialStatePropertyAll(
-//                       Color.fromARGB(255, 64, 248, 255),
-//                     ),
-//                   ),
-//                   onPressed: () async {
-//                     showModalBottomSheet(
-//                       context: context,
-//                       builder: (BuildContext context) {
-//                         return SafeArea(
-//                           child: Wrap(
-//                             children: <Widget>[
-//                               ListTile(
-//                                 leading: Icon(Icons.photo_library),
-//                                 title: Text('Choose from gallery'),
-//                                 onTap: () {
-//                                   getImage(ImageSource.gallery);
-//                                   Navigator.pop(context);
-//                                 },
-//                               ),
-//                               ListTile(
-//                                 leading: Icon(Icons.photo_camera),
-//                                 title: Text('Take a picture'),
-//                                 onTap: () {
-//                                   getImage(ImageSource.camera);
-//                                   Navigator.pop(context);
-//                                 },
-//                               ),
-//                             ],
-//                           ),
-//                         );
-//                       },
-//                     );
-//                   },
-//                   child: Text(
-//                     'Add a picture',
-//                     style: TextStyle(
-//                       color: Colors.black,
-//                       fontWeight: FontWeight.bold,
-//                       fontSize: 16,
-//                     ),
-//                   ),
-//                 ),
-//                 SizedBox(height: 20),
-//                 // Display fetched images
-//                 Column(
-//                   children: imageUrls.map((url) {
-//                     return Image.network(url);
-//                   }).toList(),
-//                 ),
-//               ],
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
